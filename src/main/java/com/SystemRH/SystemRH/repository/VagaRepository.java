2ab0232c3587673a386543bc9dd84df56f2e7d79
Cauan Oliveira <134656319+CauanO@users.noreply.github.com>
2024-11-28 17:53:53 -0300

    Update VagaRepository.java

--- src/main/java/com/SystemRH/SystemRH/repository/VagaRepository.java
+++ src/main/java/com/SystemRH/SystemRH/repository/VagaRepository.java
@@ -1,5 +1,12 @@
 package com.SystemRH.SystemRH.repository;
 
-public interface VagaRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import com.SystemRH.SystemRH.models.Vaga;
+
+public interface VagaRepository extends CrudRepository<Vaga, String> {
+	Vaga findByCodigo(long codigo);
+	List<Vaga> findByNome(String nome);
 }
