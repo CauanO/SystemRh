fc57d990e413697b8696ca13674d4c2dbc339f87
Cauan Oliveira <134656319+CauanO@users.noreply.github.com>
2024-11-28 21:14:40 -0300

    Update CandidatoRopository.java

--- src/main/java/com/SystemRH/SystemRH/repository/CandidatoRopository.java
+++ src/main/java/com/SystemRH/SystemRH/repository/CandidatoRopository.java
@@ -1,5 +1,11 @@
 package com.SystemRH.SystemRH.repository;
 
-public interface CandidatoRopository {
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import com.SystemRH.SystemRH.models.Cadidato;
+import com.SystemRH.SystemRH.models.Vaga;
 
+public interface CandidatoRopository extends CrudRepository<Cadidato, String>{
+	Iterable<Cadidato>findByVaga(Vaga vaga);
 }
